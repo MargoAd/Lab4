e26dd3da2e8c1b97e89ac012dbbfaf0b36819c10
Małgorzata Adamska <adamska.malgorzata92@gmail.com>
2023-05-07 14:02:34 +0200

    Dodanie do skryptu funkcji generowania plików z zawartością daty

--- skrypt_adamska.sh
+++ skrypt_adamska.sh
@@ -7,3 +7,12 @@ case "$1" in
                 read -p "Press Enter to exit..."
 ;;
 
+	 --logs|-l)
+        	        num_logs=${2:-100}
+        	for ((i=1; i<=$num_logs; i++)); do
+            	echo "Nazwa pliku: log$i.txt" > log$i.txt
+            	echo "Nazwa skryptu: $0" > log$i.txt
+            	date +"%Y-%m-%d" > log$i.txt
+        	done
+                read -p "Press Enter to exit..."
+        ;;
